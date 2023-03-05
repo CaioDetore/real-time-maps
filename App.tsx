f7578f8cdd1d0419b465032a13edc1c6ac123e5c
caiodetore <caio.detore@sou.unifeob.edu.br>
2023-03-05 19:39:03 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
+
+import { View } from "react-native";
+import {
+  LocationObject,
+  LocationAccuracy,
+  watchPositionAsync,
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+
+import * as S from "./styles";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>();
+
+  const mapRef = useRef<MapView>(null)
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync({
+      timeInterval: 1000,
+      distanceInterval: 1,
+      accuracy: LocationAccuracy.Highest
+    }, (response) => {
+      setLocation(response)
+      mapRef.current?.animateCamera({
+        pitch: 70,
+        center: response.coords
+      })
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={S.styles.container}>
+      {location && (
+        <MapView
+          ref={mapRef}
+          style={S.styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,24 @@
+# Localização em tempo real
+
+Esse simples projeto exibe, em um mapa, a localização do usuário em tempo real.
+
+## Feito com
+
+- Typescript
+- React Native
+
+## Bibliotecas
+
+- expo
+- expo-location
+- react-native-maps
+
+### Inicialização
+
+```
+npm install
+
+npm start
+```
+
+Projeto criado com base em uma video aula da rocketseat.
